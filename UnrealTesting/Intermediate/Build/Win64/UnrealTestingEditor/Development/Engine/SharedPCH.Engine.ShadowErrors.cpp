7bc643fa11799301066337c3697b73de140dda6c
Devin Broussard <devin.ray.broussard@gmail.com>
2022-12-06 12:29:49 -0600

    Created new project using the thirdperson template.

--- UnrealTesting/Intermediate/Build/Win64/UnrealTestingEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.30.30706
-#include "C:/Users/s218014/source/repos/unreal-engine-testing/UnrealTesting/Intermediate/Build/Win64/UnrealTestingEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- UnrealTesting/Source/UnrealTesting/UnrealTesting.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealTesting.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealTesting, "UnrealTesting" );
